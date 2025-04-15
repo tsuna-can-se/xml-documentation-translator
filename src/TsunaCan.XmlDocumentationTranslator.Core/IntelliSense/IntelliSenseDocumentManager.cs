e370185fbc30c777929391fba07b1119759ac6df
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-04-16 00:11:51 +0900

    intelli sense documentを読み込めるようにする (#10)
    * delete dummy file
    
    * プロジェクトに<RootNamespace>プロパティを追加
    
    TsunaCan.XmlDocumentationTranslator.Core.csprojファイルに、プロジェクトのルート名前空間を指定するための<RootNamespace>プロパティが追加されました。
    Test.TsunaCan.XmlDocumentationTranslator.Core.csprojファイルにも、同様に<RootNamespace>プロパティが追加されました。
    
    * パッケージ参照の追加
    
    `Directory.Packages.props` ファイルに `Maris.Logging.Testing` バージョン `1.0.0` と `Microsoft.Extensions.Logging.Abstractions` バージョン `9.0.4` を追加しました。
    `TsunaCan.XmlDocumentationTranslator.Core.csproj` ファイルに `Microsoft.Extensions.Logging.Abstractions` パッケージ参照を追加しました。
    `Test.TsunaCan.XmlDocumentationTranslator.Core.csproj` ファイルに `Maris.Logging.Testing` パッケージ参照を追加しました。
    
    * 内部メンバーのアクセス許可を追加
    
    `TsunaCan.XmlDocumentationTranslator.Core.csproj` ファイルに `<InternalsVisibleTo Include="Test.TsunaCan.XmlDocumentationTranslator.Core" />` を追加しました。この変更により、`Test.TsunaCan.XmlDocumentationTranslator.Core` アセンブリから `TsunaCan.XmlDocumentationTranslator.Core` の内部メンバーにアクセスできるようになります。
    
    * ローカライズのためのリソースファイルを追加
    
    `Messages.Designer.cs` ファイルに新しいコードを追加し、リソースクラス `Messages` を含めました。このクラスはローカライズされた文字列を検索するために使用されます。また、`Messages.resx` ファイルを追加し、ローカライズされた文字列データを含めました。さらに、`TsunaCan.XmlDocumentationTranslator.Core.csproj` ファイルに新しい `ItemGroup` を追加し、これらのリソースファイルがプロジェクトに含まれるようにしました。
    
    * 中立言語を日本語から英語に変更
    
    <NeutralLanguage> プロパティの値を `ja-JP` から `en-US` に変更しました。これにより、アセンブリの中立言語が日本語から英語に変更されます。
    
    * Add reader and writer of IntelliSense xml document.
    
    * cspellの解析対象からリソースファイル群を除外
    
    * cspell.jsonに"resx"を追加
    
    cspell.jsonファイルの"words"リストに"resx"という単語が新たに追加されました。

---
+++ src/TsunaCan.XmlDocumentationTranslator.Core/IntelliSense/IntelliSenseDocumentManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.Extensions.Logging;
+using TsunaCan.XmlDocumentationTranslator.Resources;
+
+namespace TsunaCan.XmlDocumentationTranslator.IntelliSense;
+
+/// <summary>
+///  Manages the reading and writing of IntelliSense XML documentation files.
+/// </summary>
+public class IntelliSenseDocumentManager
+{
+    private readonly XmlSerializer serializer;
+    private readonly ILogger<IntelliSenseDocumentManager> logger;
+
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="IntelliSenseDocumentManager"/> class.
+    /// </summary>
+    /// <param name="logger">The logger used for logging operations.</param>
+    public IntelliSenseDocumentManager(ILogger<IntelliSenseDocumentManager> logger)
+    {
+        this.serializer = new XmlSerializer(typeof(IntelliSenseDocument));
+        this.logger = logger;
+    }
+
+    /// <summary>
+    ///  Reads an IntelliSense XML documentation file and deserializes it into an <see cref="IntelliSenseDocument"/> object.
+    /// </summary>
+    /// <param name="intelliSenseDocumentPath">The path to the IntelliSense XML documentation file.</param>
+    /// <returns>The deserialized <see cref="IntelliSenseDocument"/> object, or <c>null</c> if the file could not be read.</returns>
+    /// <exception cref="FileNotFoundException">Thrown if <paramref name="intelliSenseDocumentPath"/> file not found.</exception>
+    /// <exception cref="InvalidOperationException">
+    ///  <list type="bullet">
+    ///   <item>Thrown if <paramref name="intelliSenseDocumentPath"/> file is not valid format.</item>
+    ///   <item>Thrown if <paramref name="intelliSenseDocumentPath"/> file is empty.</item>
+    ///  </list>
+    /// </exception>
+    internal IntelliSenseDocument? Read(string intelliSenseDocumentPath)
+    {
+        this.logger.LogDebug(Messages.XmlDocumentLoading, intelliSenseDocumentPath);
+        using var intelliSenseDocumentStreamReader = new StreamReader(intelliSenseDocumentPath);
+        var document = this.serializer.Deserialize(intelliSenseDocumentStreamReader) as IntelliSenseDocument;
+        this.logger.LogInformation(Messages.XmlDocumentLoaded, intelliSenseDocumentPath);
+        return document;
+    }
+
+    /// <summary>
+    ///  Writes an <see cref="IntelliSenseDocument"/> object to an XML file.
+    /// </summary>
+    /// <param name="outputFilePath">The path to the output XML file.</param>
+    /// <param name="document">The <see cref="IntelliSenseDocument"/> object to serialize and write.</param>
+    /// <exception cref="ArgumentException">
+    ///  <list type="bullet">
+    ///   <item>Thrown if <paramref name="outputFilePath"/> is null or empty.</item>
+    ///  </list>
+    /// </exception>
+    internal void Write(string outputFilePath, IntelliSenseDocument document)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(outputFilePath);
+
+        // Ensure the directory exists
+        var directory = Path.GetDirectoryName(outputFilePath);
+        if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+            this.logger.LogInformation(Messages.DirectoryCreated, directory);
+        }
+
+        var settings = new XmlWriterSettings
+        {
+            Indent = true,
+            IndentChars = new string(' ', 4),
+            Encoding = Encoding.UTF8,
+        };
+
+        using var writer = XmlWriter.Create(outputFilePath, settings);
+
+        // Specify empty namespaces
+        var namespaces = new XmlSerializerNamespaces();
+        namespaces.Add(string.Empty, string.Empty);
+
+        this.serializer.Serialize(writer, document, namespaces);
+        this.logger.LogInformation(Messages.XmlDocumentCreated, outputFilePath);
+    }
+}
