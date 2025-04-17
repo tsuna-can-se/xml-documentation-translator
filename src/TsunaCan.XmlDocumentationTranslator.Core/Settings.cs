717b3a14e3006082447de00c72a0ceb9375591fd
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-04-18 00:12:13 +0900

    設定クラスを追加 (#11)
    * 設定クラスとユニットテストを追加
    
    `Settings.cs`に新しい`Settings`クラスを追加し、アプリケーションの設定を表すプロパティを定義しました。これには、認証トークン、IntelliSenseドキュメントのパス、ロケール、出力ファイルのパス、ログレベル、チャットエンドポイントURL、モデルIDが含まれます。また、`ToString`メソッドをオーバーライドし、トークンの一部をマスクして表示する機能を追加しました。
    
    `SettingsTest.cs`には、`ToString`メソッドの動作をテストするユニットテストを追加し、トークンのマスク処理とプロパティの設定・取得が正しいことを確認しました。
    
    * プロパティの説明を修正
    
    `OutputFileLocale` プロパティの説明を「内部的に設定する」から「設定する」に変更し、より明確な表現にしました。

---
+++ src/TsunaCan.XmlDocumentationTranslator.Core/Settings.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+using Microsoft.Extensions.Logging;
+
+namespace TsunaCan.XmlDocumentationTranslator;
+
+/// <summary>
+///  Represents the application settings.
+/// </summary>
+public class Settings
+{
+    /// <summary>
+    ///  Gets or sets the authentication token.
+    /// </summary>
+    public required string Token { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the path to the IntelliSense document.
+    /// </summary>
+    public required string IntelliSenseDocumentPath { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the locale of the IntelliSense document.
+    /// </summary>
+    public required CultureInfo IntelliSenseDocumentLocale { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the output file path.
+    /// </summary>
+    public required string OutputFilePath { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the locale of the output file.
+    /// </summary>
+    public required CultureInfo OutputFileLocale { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the log level.
+    /// </summary>
+    public LogLevel LogLevel { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the chat endpoint URL.
+    /// </summary>
+    public required Uri ChatEndPointUrl { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the model ID to be used.
+    /// </summary>
+    public required string ModelId { get; set; }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        string tokenDisplay = this.Token.Length > 10
+            ? $"{new string('*', this.Token.Length - 5)}{this.Token[^5..]}"
+            : new string('*', this.Token.Length);
+        return $"{nameof(this.Token)}: {tokenDisplay}, " +
+            $"{nameof(this.IntelliSenseDocumentPath)}: {this.IntelliSenseDocumentPath}, " +
+            $"{nameof(this.IntelliSenseDocumentLocale)}: {this.IntelliSenseDocumentLocale}, " +
+            $"{nameof(this.OutputFilePath)}: {this.OutputFilePath}, " +
+            $"{nameof(this.OutputFileLocale)}: {this.OutputFileLocale}, " +
+            $"{nameof(this.LogLevel)}: {this.LogLevel}, " +
+            $"{nameof(this.ChatEndPointUrl)}: {this.ChatEndPointUrl}, " +
+            $"{nameof(this.ModelId)}: {this.ModelId}";
+    }
+}
