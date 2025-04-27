ab197ee74798622630e942f16fec64ae09f00104
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-04-27 15:50:47 +0900

    ITranslatorのTranslateAsyncメソッドを更新
    ITranslatorインターフェースのTranslateAsyncメソッドのdocumentパラメータをXDocument型からIntelliSenseDocumentAccessor型に変更しました。また、documentパラメータの説明も更新し、より明確にしました。

--- src/TsunaCan.XmlDocumentationTranslator.Core/ITranslator.cs
+++ src/TsunaCan.XmlDocumentationTranslator.Core/ITranslator.cs
@@ -12,15 +12,14 @@ public interface ITranslator
     /// <summary>
     ///  Translates the XML documentation file to a different languages.
     /// </summary>
-    /// <param name="document">Source IntelliSense document.</param>
+    /// <param name="document">Source IntelliSense document accessor.</param>
     /// <param name="sourceLanguage">Source document language.</param>
     /// <param name="targetLanguages"> Target document languages.</param>
     /// <returns>Translated IntelliSense documents.</returns>
     /// <exception cref="ArgumentException">
     ///  <list type="bullet">
-    ///   <item>Culture is not invaliant culture.</item>
     ///   <item><paramref name="targetLanguages"/> is empty.</item>
     ///  </list>
     /// </exception>
-    Task<Dictionary<CultureInfo, IntelliSenseDocument>> TranslateAsync(XDocument document, CultureInfo sourceLanguage, IEnumerable<CultureInfo> targetLanguages);
+    Task<Dictionary<CultureInfo, IntelliSenseDocument>> TranslateAsync(IntelliSenseDocumentAccessor document, CultureInfo sourceLanguage, IEnumerable<CultureInfo> targetLanguages);
 }
