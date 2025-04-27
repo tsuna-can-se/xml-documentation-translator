395281ff22cc943703a76854d4b709c5aac25322
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-04-27 15:08:34 +0900

    IntelliSenseDocumentAccessorのアクセス修飾子を変更
    `IntelliSenseDocumentAccessor` クラス、コンストラクタ、`GetAssemblyName` メソッド、`GetMembers` メソッドのアクセス修飾子を `internal` から `public` に変更しました。これにより、これらの要素が他のアセンブリからアクセス可能になり、外部からの利用が容易になります。

--- src/TsunaCan.XmlDocumentationTranslator.Core/IntelliSense/IntelliSenseDocumentAccessor.cs
+++ src/TsunaCan.XmlDocumentationTranslator.Core/IntelliSense/IntelliSenseDocumentAccessor.cs
@@ -5,7 +5,7 @@ namespace TsunaCan.XmlDocumentationTranslator.IntelliSense;
 /// <summary>
 ///  IntelliSense XML documentation file accessor.
 /// </summary>
-internal class IntelliSenseDocumentAccessor
+public class IntelliSenseDocumentAccessor
 {
     private readonly XDocument document;
 
@@ -13,21 +13,21 @@ internal class IntelliSenseDocumentAccessor
     /// Initializes a new instance of the <see cref="IntelliSenseDocumentAccessor"/> class.
     /// </summary>
     /// <param name="document"><see cref="XDocument"/> object.</param>
-    internal IntelliSenseDocumentAccessor(XDocument document)
+    public IntelliSenseDocumentAccessor(XDocument document)
         => this.document = document;
 
     /// <summary>
     /// Gets the assembly name element value from the XML document.
     /// </summary>
     /// <returns>Assembly name.</returns>
-    internal string GetAssemblyName()
+    public string GetAssemblyName()
         => this.document.Root?.Element(Constants.AssemblyElement)?.Element(Constants.NameElement)?.Value ?? string.Empty;
 
     /// <summary>
     /// Gets the member elements xml string list from the XML document.
     /// </summary>
     /// <returns>Member elements xml string list.</returns>
-    internal IEnumerable<string> GetMembers()
+    public IEnumerable<string> GetMembers()
         => this.document.Descendants(Constants.MemberElement)
             .Select(m => m.ToString(SaveOptions.DisableFormatting)); // get member element xml string.
 }
