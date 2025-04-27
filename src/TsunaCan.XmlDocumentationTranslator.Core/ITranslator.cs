192b8bd82aa04013a4f35b35429b6ce043c36280
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-04-27 14:37:35 +0900

    ITranslatorインターフェースを追加
    XMLドキュメントファイルを異なる言語に翻訳するためのインターフェースをITranslator.csに追加しました。翻訳対象のドキュメント、ソース言語、ターゲット言語を指定するメソッドを実装し、不正な文化や空のターゲット言語に対する例外処理の説明も追加しました。

---
+++ src/TsunaCan.XmlDocumentationTranslator.Core/ITranslator.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Xml.Linq;
+using TsunaCan.XmlDocumentationTranslator.IntelliSense;
+
+namespace TsunaCan.XmlDocumentationTranslator;
+
+/// <summary>
+///  Interface for translating XML documentation files.
+/// </summary>
+public interface ITranslator
+{
+    /// <summary>
+    ///  Translates the XML documentation file to a different languages.
+    /// </summary>
+    /// <param name="document">Source IntelliSense document.</param>
+    /// <param name="sourceLanguage">Source document language.</param>
+    /// <param name="targetLanguages"> Target document languages.</param>
+    /// <returns>Translated IntelliSense documents.</returns>
+    /// <exception cref="ArgumentException">
+    ///  <list type="bullet">
+    ///   <item>Culture is not invaliant culture.</item>
+    ///   <item><paramref name="targetLanguages"/> is empty.</item>
+    ///  </list>
+    /// </exception>
+    Task<Dictionary<CultureInfo, IntelliSenseDocument>> TranslateAsync(XDocument document, CultureInfo sourceLanguage, IEnumerable<CultureInfo> targetLanguages);
+}
