49ff1c993df21fdfda6e4c6c01a7397a6641bf87
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-05-10 14:01:36 +0900

    Program.cs を仮で追加

---
+++ src/TsunaCan.XmlDocumentationTranslator.AI/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+using TsunaCan.XmlDocumentationTranslator;
+using TsunaCan.XmlDocumentationTranslator.AI;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+///  Extension methods for adding AI translation services.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    ///  Adds AI translation services to the service collection.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/>.</param>
+    /// <param name="settings">Settings.</param>
+    /// <returns>Configured <see cref="IServiceCollection"/> object.</returns>
+    public static IServiceCollection AddAITranslator(this IServiceCollection services, Settings settings)
+    {
+        services.AddSingleton<ITranslator, AITranslator>();
+        services.AddSingleton(
+            new ChatCompletionsClient(
+                settings.ChatEndPointUrl,
+                new AzureKeyCredential(settings.Token)));
+        services.AddChatClient(
+            services => services.GetRequiredService<ChatCompletionsClient>()
+            .AsIChatClient(settings.ModelId))
+        .UseLogging();
+        return services;
+    }
+}
--- src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
+++ src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TsunaCan.XmlDocumentationTranslator;
+using TsunaCan.XmlDocumentationTranslator.IntelliSense;
+
+var builder = Host.CreateApplicationBuilder();
+var settings = (Settings)null!; //EnvironmentReader.GetSettings();
+builder.Services.AddSingleton(settings);
+builder.Services.AddSingleton<TranslationService>();
+builder.Services.AddSingleton<IIntelliSenseDocumentManager, IntelliSenseDocumentManager>();
+builder.Services.AddAITranslator(settings);
+builder.Services.AddLogging(b => b.SetMinimumLevel(settings.LogLevel));
+
+var app = builder.Build();
+
+var translationService = app.Services.GetRequiredService<TranslationService>();
+await translationService.ExecuteAsync();
