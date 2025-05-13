7eb07b14aba1ab303dd641c8ff5d015c316ae62d
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-05-13 17:39:30 +0900

    AI翻訳サービスの設定方法を変更
    `ServiceCollectionExtensions.cs`において、`using`ディレクティブに`Microsoft.Extensions.Configuration`を追加し、`configureOptions`パラメータを`configuration`パラメータに変更しました。これにより、AI翻訳サービスを追加するメソッドが`IConfiguration`オブジェクトを使用して設定をバインドし、データ注釈の検証を行うようになりました。設定管理がより柔軟になり、アプリケーションの設定を簡単に扱えるようになります。

--- src/TsunaCan.XmlDocumentationTranslator.Core/ServiceCollectionExtensions.cs
+++ src/TsunaCan.XmlDocumentationTranslator.Core/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using TsunaCan.XmlDocumentationTranslator.IntelliSense;
 
 namespace TsunaCan.XmlDocumentationTranslator;
@@ -12,11 +13,13 @@ public static class ServiceCollectionExtensions
     ///  Adds AI translation services to the service collection.
     /// </summary>
     /// <param name="services"><see cref="IServiceCollection"/>.</param>
-    /// <param name="configureOptions">Configure settings.</param>
+    /// <param name="configuration"><see cref="IConfiguration"/> objects.</param>
     /// <returns>Configured <see cref="IServiceCollection"/> object.</returns>
-    public static IServiceCollection AddTranslatorServices(this IServiceCollection services, Action<Settings> configureOptions)
+    public static IServiceCollection AddTranslatorServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure(configureOptions);
+        services.AddOptionsWithValidateOnStart<Settings>()
+            .Bind(configuration)
+            .ValidateDataAnnotations();
         services.AddSingleton<TranslationService>();
         services.AddSingleton<IIntelliSenseDocumentManager, IntelliSenseDocumentManager>();
         return services;
