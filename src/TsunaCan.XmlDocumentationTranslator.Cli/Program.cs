89a630e3cfe395b281fea6c074cd77cd5f3e22c5
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-05-13 09:32:29 +0900

    サンプルのXMLドキュメントとエントリーポイントを追加

--- src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
+++ src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
@@ -1,15 +1,34 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Globalization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TsunaCan.XmlDocumentationTranslator;
-using TsunaCan.XmlDocumentationTranslator.IntelliSense;
+using TsunaCan.XmlDocumentationTranslator.AI;
 
 var builder = Host.CreateApplicationBuilder();
-var settings = (Settings)null!; //EnvironmentReader.GetSettings();
+var settings = new Settings()
+{
+    SourceDocumentPath = "TsunaCan.HelloWorld.xml",
+    SourceDocumentLanguage = new CultureInfo("ja"),
+    OutputDirectoryPath = "output",
+    OutputFileLanguages = [new CultureInfo("en")],
+    LogLevel = LogLevel.Debug,
+};
+var aiSettings = new AISettings()
+{
+    ChatEndPointUrl = new Uri("https://hoge.com"),
+    ModelId = "huge",
+    Token = "token",
+};
 builder.Services.AddSingleton(settings);
-builder.Services.AddSingleton<TranslationService>();
-builder.Services.AddSingleton<IIntelliSenseDocumentManager, IntelliSenseDocumentManager>();
-builder.Services.AddAITranslator(settings);
+builder.Services.AddAITranslator(aiSettings);
+builder.Services.AddTranslatorServices(option =>
+{
+    option.SourceDocumentPath = settings.SourceDocumentPath;
+    option.SourceDocumentLanguage = settings.SourceDocumentLanguage;
+    option.OutputDirectoryPath = settings.OutputDirectoryPath;
+    option.OutputFileLanguages = settings.OutputFileLanguages;
+});
 builder.Services.AddLogging(b => b.SetMinimumLevel(settings.LogLevel));
 
 var app = builder.Build();
