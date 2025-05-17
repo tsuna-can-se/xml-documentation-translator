0fde22c7aa0e5a5e775276f70305841ea268b249
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-05-17 14:58:49 +0900

    設定管理の方法を改善
    `Program.cs`でハードコーディングされた設定オブジェクトを削除し、環境変数やコマンドライン引数から設定を読み込むように変更しました。これにより、アプリケーションの設定が柔軟に管理できるようになり、実行時に異なる設定を適用しやすくなりました。また、ログレベルの設定も動的に取得されるようになりました。

--- src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
+++ src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -6,30 +6,18 @@ using TsunaCan.XmlDocumentationTranslator;
 using TsunaCan.XmlDocumentationTranslator.AI;
 
 var builder = Host.CreateApplicationBuilder();
-var settings = new Settings()
-{
-    SourceDocumentPath = "TsunaCan.HelloWorld.xml",
-    SourceDocumentLanguage = new CultureInfo("ja"),
-    OutputDirectoryPath = "output",
-    OutputFileLanguages = [new CultureInfo("en")],
-    LogLevel = LogLevel.Debug,
-};
-var aiSettings = new AISettings()
-{
-    ChatEndPointUrl = new Uri("https://hoge.com"),
-    ModelId = "huge",
-    Token = "token",
-};
-builder.Services.AddSingleton(settings);
-builder.Services.AddAITranslator(aiSettings);
-builder.Services.AddTranslatorServices(option =>
-{
-    option.SourceDocumentPath = settings.SourceDocumentPath;
-    option.SourceDocumentLanguage = settings.SourceDocumentLanguage;
-    option.OutputDirectoryPath = settings.OutputDirectoryPath;
-    option.OutputFileLanguages = settings.OutputFileLanguages;
-});
-builder.Services.AddLogging(b => b.SetMinimumLevel(settings.LogLevel));
+
+builder.Configuration
+    .AddEnvironmentVariables()
+    .AddCommandLine(args);
+
+var logLevel = builder.Configuration.GetValue(nameof(LogLevel), LogLevel.Information);
+var coreSettingsConfiguration = builder.Configuration.GetSection(nameof(CoreSettings));
+var aiSettingsConfiguration = builder.Configuration.GetSection(nameof(AISettings));
+
+builder.Services.AddTranslatorServices(coreSettingsConfiguration);
+builder.Services.AddAITranslator(aiSettingsConfiguration);
+builder.Services.AddLogging(b => b.SetMinimumLevel(logLevel));
 
 var app = builder.Build();
 
