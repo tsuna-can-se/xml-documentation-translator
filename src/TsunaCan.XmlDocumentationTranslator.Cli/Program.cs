28990b4d7fa545ed8d732e0f5164047db67cdb7c
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-06-01 17:39:32 +0900

    SourceDocumentLanguageが未指定の時も動作するように修正

--- src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
+++ src/TsunaCan.XmlDocumentationTranslator.Cli/Program.cs
@@ -18,8 +18,7 @@ var logLevel = builder.Configuration.GetValue(nameof(LogLevel), LogLevel.Informa
 var sourceDocumentPath = builder.Configuration.GetValue<string>("SourceDocumentPath");
 ParameterNotSetException.ThrowIfNotSet(sourceDocumentPath, "SourceDocumentPath");
 var sourceDocumentLanguageStr = builder.Configuration.GetValue<string>("SourceDocumentLanguage");
-ParameterNotSetException.ThrowIfNotSet(sourceDocumentLanguageStr, "SourceDocumentLanguage");
-var sourceDocumentLanguage = ConvertTo(sourceDocumentLanguageStr.Trim());
+var sourceDocumentLanguage = sourceDocumentLanguageStr != null ? ConvertTo(sourceDocumentLanguageStr.Trim()) : null;
 var outputDirectoryPath = builder.Configuration.GetValue("OutputDirectoryPath", string.Empty);
 var outputFileLanguagesStr = builder.Configuration.GetValue<string>("OutputFileLanguages");
 ParameterNotSetException.ThrowIfNotSet(outputFileLanguagesStr, "OutputFileLanguages");
